6d426929f95f62babb673eafa531bbfdbe194eef
jangiwalia <jangiwalia83@gmail.com>
2023-02-12 11:08:44 -0800

    add basic auth and resetting in form

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,17 +1,55 @@
 import Card from "./Card";
-
+import { useState } from "react";
 export const User = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (name.trim().length === 0 || +age === 0) {
+      return;
+    }
+
+    if (+age < 1) {
+      return;
+    }
+
+    console.log(`${name} is ${age} years old`);
+    setName("");
+    setAge(0);
+  };
+
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleAge = (event) => {
+    setAge(event.target.value);
+  };
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex flex-col justify-between p-2">
           <label htmlFor="username">Enter Username</label>
-          <input type="text" id="username" placeholder="Username" />
+          <input
+            onChange={handleName}
+            type="text"
+            id="username"
+            placeholder="Username"
+            value={name}
+          />
         </div>
 
         <div className="flex flex-col justify-between p-2">
           <label htmlFor="age">Enter Your Age</label>
-          <input type="text" id="age" placeholder="Age" />
+          <input
+            type="number"
+            id="age"
+            placeholder="Age"
+            onChange={handleAge}
+            value={age}
+          />
         </div>
 
         <button type="submit" className="bg-indigo-500 mt-5 rounded-2xl w-20">
